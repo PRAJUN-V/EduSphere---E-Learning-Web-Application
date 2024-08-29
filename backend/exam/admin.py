3b28c92d1a851e61d57a49de3f2e57091b1d2649
prajun v <prajunvelandi@gmail.com>
2024-08-29 16:13:14 +0530

    commit just before implementing exam

--- backend/exam/admin.py
+++ backend/exam/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
-
-# Register your models here.
+# from django.contrib import admin
+# from .models import Exam, Question, Option, StudentExam, StudentAnswer
+#
+# admin.site.register(Exam)
+# admin.site.register(Question)
+# admin.site.register(Option)
+# admin.site.register(StudentExam)
+# admin.site.register(StudentAnswer)
